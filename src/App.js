4364659a483da5f5154d9d47f03d90b4d739ec68
Djoumoi Hassan <moy_hash@hotmail.com>
2023-02-26 20:30:53 +0100

    modifier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+  <h1 className="title">Komoria.net</h1>
+  <h2>Djoumoi Hassan 2023</h2>
+</div>
   );
 }
-
+//<!--lllllllllll -->
+//<img src="{/*logo*/}" className="App-logo" alt="logo" />
 export default App;
