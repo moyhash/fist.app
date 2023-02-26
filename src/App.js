b2e30489821cbc3e4488441a967760ba1e3d08f5
Djoumoi Hassan <moy_hash@hotmail.com>
2023-02-26 21:32:48 +0100

    Modification

--- src/App.js
+++ src/App.js
@@ -3,12 +3,117 @@
 
 function App() {
   return (
-<div>
-  <h1 className="title">Komoria.net</h1>
-  <h2>Djoumoi Hassan 2023</h2>
-</div>
+    <div>
+      <h1 className="title">Komoria.net</h1>
+      <h2>Djoumoi Hassan 2023</h2>
+
+      <table>
+        <caption>
+          <h2>Carte des clés Personnelles</h2>
+        </caption>
+
+        <tr>
+          <td>/</td>
+          <th>1</th>
+          <th>2</th>
+          <th>3</th>
+          <th>4</th>
+          <th>5</th>
+          <th>6</th>
+          <th>7</th>
+          <th>8</th>
+        </tr>
+        <tr>
+          <th>A</th>
+          <td>3105</td>
+          <td>1536</td>
+          <td>1395</td>
+          <td>6908</td>
+          <td>9040</td>
+          <td>7033</td>
+          <td>5877</td>
+          <td>4731</td>
+        </tr>
+        <tr>
+          <th>B</th>
+          <td>4756</td>
+          <td>3176</td>
+          <td>5496</td>
+          <td>5866</td>
+          <td>1751</td>
+          <td>2588</td>
+          <td>8547</td>
+          <td>6754</td>
+        </tr>
+        <tr>
+          <th>C</th>
+          <td>4494</td>
+          <td>9459</td>
+          <td>7434</td>
+          <td>7486</td>
+          <td>4899</td>
+          <td>6901</td>
+          <td>6675</td>
+          <td>0421</td>
+        </tr>
+        <tr>
+          <th>D</th>
+          <td>6491</td>
+          <td>4080</td>
+          <td>9653</td>
+          <td>9092</td>
+          <td>1752</td>
+          <td>5381</td>
+          <td>8443</td>
+          <td>5106</td>
+        </tr>
+        <tr>
+          <th>E</th>
+          <td>8077</td>
+          <td>3506</td>
+          <td>1118</td>
+          <td>0743</td>
+          <td>6653</td>
+          <td>1003</td>
+          <td>7300</td>
+          <td>6656</td>
+        </tr>
+        <tr>
+          <th>F</th>
+          <td>3882</td>
+          <td>5901</td>
+          <td>5759</td>
+          <td>9533</td>
+          <td>4242</td>
+          <td>9231</td>
+          <td>5708</td>
+          <td>0118</td>
+        </tr>
+        <tr>
+          <th>G</th>
+          <td>0738</td>
+          <td>5396</td>
+          <td>4604</td>
+          <td>2550</td>
+          <td>6028</td>
+          <td>6177</td>
+          <td>9361</td>
+          <td>8455</td>
+        </tr>
+        <tr>
+          <th>H</th>
+          <td>4993</td>
+          <td>4344</td>
+          <td>3826</td>
+          <td>9948</td>
+          <td>0823</td>
+          <td>1009</td>
+          <td>5790</td>
+          <td>6682</td>
+        </tr>
+      </table>
+    </div>
   );
 }
-//<!--lllllllllll -->
-//<img src="{/*logo*/}" className="App-logo" alt="logo" />
+
 export default App;
